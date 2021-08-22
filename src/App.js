9328cbd1cb90280180736e6b2a9b8c15c226a323
Fakhri Rizha Ananda <mr.fakhririzhaa@gmail.com>
2021-08-22 15:35:48 +0700

    Lecture 37

--- src/App.js
+++ src/App.js
@@ -4,21 +4,26 @@ function App() {
 	const expenses = [
 		{
 			id: "e1",
-			title: "Car Insurance",
-			amount: 294.67,
+			title: "Asuransi Mobil",
+			amount: 250000,
 			date: new Date(2021, 2, 28),
 		},
-		{ id: "e2", title: "New TV", amount: 594.67, date: new Date(2021, 2, 27) },
+		{
+			id: "e2",
+			title: "TV Baru",
+			amount: 4799000,
+			date: new Date(2021, 2, 27),
+		},
 		{
 			id: "e3",
-			title: "Health Bills",
-			amount: 94.67,
+			title: "Iuran BPJS",
+			amount: 150000,
 			date: new Date(2021, 2, 26),
 		},
 		{
 			id: "e4",
-			title: "Electricity",
-			amount: 294.67,
+			title: "Listrik",
+			amount: 375895,
 			date: new Date(2021, 2, 25),
 		},
 	];
@@ -28,22 +33,22 @@ function App() {
 			<ExpenseItem
 				title={expenses[0].title}
 				amount={expenses[0].amount}
-				date={expenses[0].date.toISOString()}
+				date={expenses[0].date}
 			></ExpenseItem>
 			<ExpenseItem
 				title={expenses[1].title}
 				amount={expenses[1].amount}
-				date={expenses[1].date.toISOString()}
+				date={expenses[1].date}
 			></ExpenseItem>
 			<ExpenseItem
 				title={expenses[2].title}
 				amount={expenses[2].amount}
-				date={expenses[2].date.toISOString()}
+				date={expenses[2].date}
 			></ExpenseItem>
 			<ExpenseItem
 				title={expenses[3].title}
 				amount={expenses[3].amount}
-				date={expenses[3].date.toISOString()}
+				date={expenses[3].date}
 			></ExpenseItem>
 		</div>
 	);
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,20 @@
 import "./ExpensiveItem.css";
 
 function ExpenseItem(props) {
-	const expenseDate = new Date(2021, 2, 27);
-	const expenseTitle = "Car Insurance";
-	const expenseAmount = 294.67;
+	const month = props.date.toLocaleString("id-ID", { month: "long" });
+	const day = props.date.toLocaleString("id-ID", { day: "2-digit" });
+	const year = props.date.getFullYear();
 
 	return (
 		<div className="expense-item">
-			<div>{props.date}</div>
+			<div>
+				<div>{month}</div>
+				<div>{year}</div>
+				<div>{day}</div>
+			</div>
 			<div className="expense-item__description">
 				<h2>{props.title}</h2>
-				<div className="expense-item__price">${props.amount}</div>
+				<div className="expense-item__price">Rp. {props.amount}</div>
 			</div>
 		</div>
 	);
