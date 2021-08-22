70674db0a8cce569f5be1d274794cbdbb89e0051
Fakhri Rizha Ananda <mr.fakhririzhaa@gmail.com>
2021-08-22 15:03:44 +0700

    Lecture 34

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import "./ExpensiveItem.css";
 
 function ExpenseItem() {
+	const expenseDate = new Date(2021, 2, 27);
+	const expenseTitle = "Car Insurance";
+	const expenseAmount = 294.67;
+
 	return (
 		<div className="expense-item">
 			<div>March 27th 2021</div>
 			<div className="expense-item__description">
-				<h2>Car Insurance</h2>
-				<div className="expense-item__price">$294.67</div>
+				<h2>{expenseTitle}</h2>
+				<div className="expense-item__price">${expenseAmount}</div>
 			</div>
 		</div>
 	);
