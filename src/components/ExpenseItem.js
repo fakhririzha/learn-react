abea88a26a59e34bc029c8b13d9184198979ff55
Fakhri Rizha Ananda <mr.fakhririzhaa@gmail.com>
2021-08-22 14:53:45 +0700

    Lecture 34

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import "./ExpensiveItem.css";
+
 function ExpenseItem() {
 	return (
-		<div>
+		<div className="expense-item">
 			<div>March 27th 2021</div>
-			<div>
+			<div className="expense-item__description">
 				<h2>Car Insurance</h2>
-				<div>$294.67</div>
+				<div className="expense-item__price">$294.67</div>
 			</div>
 		</div>
 	);
