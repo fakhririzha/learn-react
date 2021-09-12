6e7287dbaed1012853298c5380a662aa97683f28
Fakhri Rizha Ananda <mr.fakhririzhaa@gmail.com>
2021-09-12 18:58:29 +0700

    Lecture 70

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -3,7 +3,7 @@ import "./ChartBar.css";
 const ChartBar = (props) => {
 	let barFillHeight = "0%";
 
-	if (props.max > 0) {
+	if (props.maxValue > 0) {
 		barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
 	}
 	return (
@@ -14,7 +14,7 @@ const ChartBar = (props) => {
 					style={{ height: barFillHeight }}
 				></div>
 			</div>
-			<div className="chart-bar__label"></div>
+			<div className="chart-bar__label">{props.label}</div>
 		</div>
 	);
 };
