e1babae1914a2ba2c12ab95b7d02b121f60175f8
Fakhri Rizha Ananda <mr.fakhririzhaa@gmail.com>
2021-09-12 10:40:50 +0700

    Lecture 64

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
+import { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+	{
+		id: "e1",
+		title: "Asuransi Mobil",
+		amount: 250000,
+		date: new Date(2021, 2, 28),
+	},
+	{
+		id: "e2",
+		title: "TV Baru",
+		amount: 4799000,
+		date: new Date(2021, 2, 27),
+	},
+	{
+		id: "e3",
+		title: "Iuran BPJS",
+		amount: 150000,
+		date: new Date(2021, 2, 26),
+	},
+	{
+		id: "e4",
+		title: "Listrik",
+		amount: 375895,
+		date: new Date(2021, 2, 25),
+	},
+];
+
 const App = () => {
-	const expenses = [
-		{
-			id: "e1",
-			title: "Asuransi Mobil",
-			amount: 250000,
-			date: new Date(2021, 2, 28),
-		},
-		{
-			id: "e2",
-			title: "TV Baru",
-			amount: 4799000,
-			date: new Date(2021, 2, 27),
-		},
-		{
-			id: "e3",
-			title: "Iuran BPJS",
-			amount: 150000,
-			date: new Date(2021, 2, 26),
-		},
-		{
-			id: "e4",
-			title: "Listrik",
-			amount: 375895,
-			date: new Date(2021, 2, 25),
-		},
-	];
+	const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
 	const addExpenseHandler = (expense) => {
-		console.log("In App.js");
-		console.log(expense);
+		setExpenses((prevExpenses) => {
+			return [expense, ...prevExpenses];
+		});
 	};
 
 	// FIXME Ini adalah syntax jika menggunakan syntax React versi lama. Tapi harus import React from "react";
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -8,7 +8,7 @@ const ExpenseItem = (props) => {
 	console.log("ExpenseItem evaluated by React");
 
 	const clickHandler = () => {
-		setTitle("Updated!");
+		// setTitle("Updated!");
 		console.log(title);
 	};
 
@@ -16,7 +16,7 @@ const ExpenseItem = (props) => {
 		<Card className="expense-item">
 			<ExpenseDate date={props.date} />
 			<div className="expense-item__description">
-				<h2>{title}</h2>
+				<h2>{props.title}</h2>
 				<div className="expense-item__price">Rp. {props.amount}</div>
 			</div>
 			<button onClick={clickHandler}>Ubah Judul</button>
